a7915e3d6d8bab103eb678481eca7f34cd5c9499
rahwang <rah1@uchicago.edu>
2016-12-07 23:31:48 -0500

    Adding prototypes for many features

---
+++ Assets/FaceHeadset.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceHeadset : MonoBehaviour {
+
+    private GameManager game_manager;
+
+	// Use this for initialization
+	void Start () {
+        game_manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform target = game_manager.headset_trans;
+        Vector3 lookDir = target.position - transform.position;
+        Vector3 lookPoint = target.position - transform.position;
+        lookPoint.y = target.position.y;
+        transform.LookAt(lookPoint);
+	}
+}
---
+++ Assets/TriggerMana.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerMana : MonoBehaviour {
+
+    public GameObject manaSphere;
+    public GameObject spellBook;
+
+    private SteamVR_Controller.Device device;
+    private SteamVR_TrackedObject tracked_object;
+
+    // Use this for initialization
+    void Start () {
+        tracked_object = GetComponent<SteamVR_TrackedObject>();
+        spellBook.SetActive(false);
+    }
+
+    void SpellbookEnable()
+    {
+        //manaSphere.SetActive(false);
+        spellBook.SetActive(true);
+    }
+
+    void SpellbookDisable()
+    {
+        //manaSphere.SetActive(true);
+        spellBook.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        device = SteamVR_Controller.Input((int)tracked_object.index);
+
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("turn on spell");
+            SpellbookEnable();
+        }
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("turn off spell");
+            SpellbookDisable();
+        }
+    }
+}
